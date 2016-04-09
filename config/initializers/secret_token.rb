0d0d3bc5564aec1637fe6c29e78f64f2c1944dd3
Sean Lovinger <lovingersean01@gmail.com>
2016-04-09 23:44:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWeather::Application.config.secret_key_base = '21c8d634246eadc2c778a1a4c2ab661afedb5d9c1691cf9f897f81638f1bb33fc2c341cfcdedeb42f1dfe6706e58b54077cc8c724054323bcfab5fce762b9f38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyWeather::Application.config.session_store :cookie_store, key: '_my_weather_session'

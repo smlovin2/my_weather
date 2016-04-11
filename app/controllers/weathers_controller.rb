5697cf584612e135f1189caaa97cebfe028dd3b8
Sean Lovinger <lovingersean01@gmail.com>
2016-04-11 18:41:48 +0000

    Added a flash error message for invalid zip codes, added a background image

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,6 +1,11 @@
 class WeathersController < ApplicationController
   def show
     @response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode_param + ',us&units=imperial&appid=' + ENV['open_weather_map_api_key'])
+    
+    if @response['cod'] == "404"
+      flash[:error] = '<strong>' + @response["message"] + '</strong> Please enter a valid zipcode (ex. 94040).'
+      redirect_to root_path
+    end
   end
   
   private
--- spec/controllers/weathers_controller_spec.rb
+++ spec/controllers/weathers_controller_spec.rb
@@ -14,13 +14,21 @@ RSpec.describe WeathersController, type: :controller do
                 "id":0,
                 "name":"Mountain View",
                 "cod":200}'
-    stub_request(:any, /api.openweathermap.org/).to_return(body: response, status: 200, headers: {})
+    stub_request(:any, /api.openweathermap.org/).to_return(body: response, status: 200, headers: {content_type: 'application/json'})
   end
 
   describe "GET #show" do
-    it "returns http success" do
+    it "returns http success when response has success code" do
       get :show, zipcode: 94040
       expect(response).to have_http_status(:success)
+    end
+    
+    it "redirects to the root page if the response has a 404 code" do
+      error_response = '{"cod":"404","message":"Error: Not found city"}'
+      stub_request(:any, /api.openweathermap.org/).to_return(body: error_response, status: 200, headers: {content_type: 'application/json'})
+      get :show, zipcode: "sdlfak;le"
+      
+      expect(response).to redirect_to(root_path)
     end
     
     it "gets sets 'response' to be the response from the api call" do
